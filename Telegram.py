ad1fd4c4312e2c6c3a4687597aa0022e80152146
Mikhail <mikebarskiy@gmail.com>
2019-11-09 23:19:13 +0300

    Initial commit

---
+++ Telegram.py
@@ -0,0 +1,61 @@
+import logging
+import threading
+from time import sleep, time
+
+from telegram.ext import CommandHandler
+from telegram.ext import Updater
+
+from YoutubeReceiver import get_last_video
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Nice to meet you")
+
+
+def last_video(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=get_last_video())
+
+
+def init_handlers():
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    last_video_handler = CommandHandler('last_video', last_video)
+    dispatcher.add_handler(last_video_handler)
+
+
+def start_listening_activities():
+    video = get_last_video()
+    sleep_time = 15
+
+    six_hours_sleep = 6 * 60 * 60
+
+    while True:
+        sleep(sleep_time - time() % sleep_time)
+        new_video = get_last_video()
+
+        if new_video and video != new_video:
+            video = new_video
+            logger.info("New video was uploaded")
+
+            sleep_time = six_hours_sleep
+        else:
+            logger.info("New video was not uploaded")
+            sleep_time = 15
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    logger = logging.getLogger(__name__)
+
+    updater = Updater(token='', use_context=True)
+    dispatcher = updater.dispatcher
+
+    init_handlers()
+
+    check_updates_thread = threading.Thread(target=start_listening_activities)
+    check_updates_thread.start()
+
+    updater.start_polling()
---
+++ YoutubeReceiver.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.activities.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+
+import googleapiclient.discovery
+
+
+def send_request(channel_id):
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = ""
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY, cache_discovery=False)
+
+    request = youtube.activities().list(
+        part="contentDetails",
+        channelId=channel_id,
+        maxResults=1
+    )
+    return request.execute()
+
+
+def get_last_video():
+    eliseev_channel_id = "UCSdSBG3MRqhiZv4S1wO39SQ"
+
+    response = send_request(eliseev_channel_id)
+
+    activity = response['items'][0]
+    content_info = activity['contentDetails']
+
+    if "upload" in content_info:
+        video_id = content_info['upload']['videoId']
+        main_part = "https://www.youtube.com/watch?v="
+
+        return main_part + video_id
